94622dc8981b5a5e4f66aae020dea41a6f003a74
Gustavo Caso <gustavo.caso@shopify.com>
2022-10-19 18:27:37 +0200

    testing rollbacks part 3

--- src/components/Welcome.server.jsx
+++ src/components/Welcome.server.jsx
@@ -156,7 +156,7 @@ export default function Welcome() {
           Hello, Hydrogen
         </h1>
         <p className="text-lg mb-8">
-          Welcome to your custom storefront. Gustavo Caso. Let&rsquo;s get building. Testing updates in core and rollbacks part 2
+          Welcome to your custom storefront. Gustavo Caso. Let&rsquo;s get building. Testing updates in core and rollbacks part 3
         </p>
         <div className="flex flex-col lg:flex-row justify-center items-center gap-8 text-gray-700">
           <DocsButton
